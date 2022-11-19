8c4ff0a620131e2b2dab220425564f2d71da9201
Mason Hirst <mason.j.hirst@gmail.com>
2022-11-18 17:11:01 -0700

    mm

--- server/controllers.js
+++ server/controllers.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.HEROKU_DB_URI, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
