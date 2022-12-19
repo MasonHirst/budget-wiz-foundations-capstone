62e340500e0e59dc57201ec95a615271d6707df4
Mason Hirst <mason.j.hirst@gmail.com>
2022-12-19 12:12:03 -0700

    m

--- server/server.js
+++ server/server.js
@@ -36,6 +36,5 @@ app.post('/getCategoryId', getCategoryId)
 
 
 
-const PORT = process.env.PORT
-
+const PORT = process.env.PORT || 3999
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
